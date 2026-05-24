cce9bba7ca47ce453f0c0344cc3288017b160de8
daniel.strielnikow <daniel.strielnikow@hrti.com.pl>
2026-05-24 15:18:06 +0200

    feat: implement Keycloak integration in auth-service

---
+++ services/auth-service/src/main/java/com/streamhub/authservice/infrastructure/keylock/KeycloakService.java
@@ -0,0 +1,112 @@
+package com.streamhub.authservice.infrastructure.keylock;
+
+import com.streamhub.authservice.dto.reponse.TokenResponse;
+import com.streamhub.authservice.dto.request.RegisterRequest;
+import jakarta.ws.rs.core.Response;
+import lombok.RequiredArgsConstructor;
+import org.keycloak.admin.client.CreatedResponseUtil;
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.representations.idm.CredentialRepresentation;
+import org.keycloak.representations.idm.RoleRepresentation;
+import org.keycloak.representations.idm.UserRepresentation;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestClient;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class KeycloakService {
+    private final Keycloak keycloak;
+    private final RestClient restClient;
+
+    @Value("${keycloak.url}")
+    private String keycloakUrl;
+
+    @Value("${keycloak.realm}")
+    private String realm;
+
+    @Value("${keycloak.client-id}")
+    private String clientId;
+
+    @Value("${keycloak.client-secret}")
+    private String clientSecret;
+    
+    public UUID registerUser(RegisterRequest request) {
+        UserRepresentation user = getUserRepresentation(request);
+
+        Response response = keycloak.realm(realm).users().create(user);
+        
+        String userId = CreatedResponseUtil.getCreatedId(response);
+
+        RoleRepresentation role = keycloak.realm(realm)
+                .roles()
+                .get("ROLE_USER")
+                .toRepresentation();
+        
+        keycloak.realm(realm)
+                .users()
+                .get(userId)
+                .roles()
+                .realmLevel()
+                .add(List.of(role));
+
+        return UUID.fromString(userId);
+    }
+
+    private static UserRepresentation getUserRepresentation(RegisterRequest request) {
+        CredentialRepresentation credential = new CredentialRepresentation();
+        credential.setType(CredentialRepresentation.PASSWORD);
+        credential.setValue(request.password());
+        credential.setTemporary(false);
+
+        UserRepresentation user = new UserRepresentation();
+
+        user.setUsername(request.email());
+        user.setEmail(request.email());
+        user.setFirstName(request.firstName());
+        user.setLastName(request.lastName());
+        user.setEnabled(true);
+        user.setEmailVerified(true);
+        user.setCredentials(List.of(credential));
+        return user;
+    }
+
+    public TokenResponse getToken(String email, String password) {
+        MultiValueMap<String, String> form = new LinkedMultiValueMap<>();
+        form.add("grant_type", "password");
+        form.add("client_id", clientId);
+        form.add("client_secret", clientSecret);
+        form.add("username", email);
+        form.add("password", password);
+
+        return restClient.post()
+                .uri(keycloakUrl + "/realms/" + realm + "/protocol/openid-connect/token")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .body(form)
+                .retrieve()
+                .body(TokenResponse.class);
+    }
+
+    public TokenResponse refreshToken(String refreshToken) {
+
+        MultiValueMap<String, String> form = new LinkedMultiValueMap<>();
+        form.add("grant_type", "refresh_token");
+        form.add("client_id", clientId);
+        form.add("client_secret", clientSecret);
+        form.add("refresh_token", refreshToken);
+
+        return restClient.post()
+                .uri(keycloakUrl + "/realms/" + realm + "/protocol/openid-connect/token")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .body(form)
+                .retrieve()
+                .body(TokenResponse.class);
+    }
+    
+}
